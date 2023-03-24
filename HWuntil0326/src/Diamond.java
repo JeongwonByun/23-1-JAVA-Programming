eb2b319d63870e89abcaddffcd4ccefc8dfa9ec2
JeongwonByun <jwbyun0629@naver.com>
2023-03-24 09:12:02 +0900

    230324-1

---
+++ HWuntil0326/src/Diamond.java
@@ -0,0 +1,16 @@
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<4;i++) {
+			for(int b=4;b>0;b--)
+				System.out.print(" ");
+			for(int u=1;u<5;u=(u*2)-1)
+				System.out.print("*");
+			
+			System.out.print("\n");
+		}
+		
+	}
+}
--- HWuntil0326/src/TimesTable.java
+++ HWuntil0326/src/TimesTable.java
@@ -3,7 +3,13 @@ public class TimesTable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int k=1,i=2;
+		for( k=1; k<10;k++) {
+			for( i=2; i<6; i++) {
+				System.out.print(i+"X"+k+"="+(i*k)+" ");
+			}
+		System.out.print("\n");
+		}
 	}
 
 }
