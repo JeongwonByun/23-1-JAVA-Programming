682aa65a7cd97a37a0baca28755ee4765dc23f7b
JeongwonByun <jwbyun0629@naver.com>
2023-03-24 09:08:47 +0900

    230324

---
+++ HWuntil0326/src/RockScissorPaper.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RockScissorPaper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("가위(0) 바위(1) 보(2) 중 숫자를 입력하시오. : ");
+		int user = sc.nextInt();
+		
+		int computer = (int) (Math.random()*3);
+		if(user==computer)
+			System.out.println("인간:" + user + " 컴퓨터:" + computer + " 비겼습니다");
+		else if(user==(computer+1)%3)
+			System.out.println("인간:" + user + " 컴퓨터:" + computer + " 인간 승리");
+		else
+			System.out.println("인간:" + user + " 컴퓨터:" + computer + " 컴퓨터 승리");
+
+		
+	}
+
+}
---
+++ HWuntil0326/src/Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0, value=0;
+		while(value!=-1) {
+			sum = sum + value;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("정수를 입력하시오 : ");
+			value = sc.nextInt();
+		}
+		System.out.println("정수의 합은 " + sum + " 입니다.");
+	}
+
+}
